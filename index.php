f1401548431af82c94f47bc572ad8dd6de2f3673
LucasGreissl <130068605+LucasGreissl@users.noreply.github.com>
2023-05-10 11:24:56 +0200

    Update index.php
    Benutzername in Datenbank einschreiben und auf die nächste Seite weiterleiten

--- index.php
+++ index.php
@@ -7,27 +7,34 @@
 </head>
 
 <body>
-    <form action="frage1.php" method="post">
-        <label for="benutzername">Benutzername: </label>
-        <input type="text" id="name" name="nameInput">
-        <input type="submit" value="Login und Quiz starten">
-    </form>
+        <form action="Index.php" method="post">
 
-    
+            <label for="benutzername">Benutzername: </label>
+            <input type="text" id="name" name="nameInput">
+            <input type="submit" value="Login und Quiz starten">
+
+        </form>
 <?php
-if (!empty ($_POST)){
+session_start();
+
+if (!empty($_POST)) {
     $name = $_POST['nameInput'];
 
-    try{
+    try {
         $dsn = 'mysql:host=localhost;dbname=quiz;charset=utf8mb4';
         $username = 'root';
         $password = '';
         $dbh = new \PDO($dsn, $username, $password);
-        
-        $statement = $dbh->prepare("INSERT INTO quizdaten(benutzername, frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, endergebnis) VALUES(?,?,?,?,?,?,?,?,?,?)");
-        $statement->execute(array($name,true,false,true,true,true,true,true,true,8));   
-    }catch(\Throwable $e){
 
+        $statement = $dbh->prepare("INSERT INTO quizdaten(benutzername, frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, endergebnis) VALUES(?,?,?,?,?,?,?,?,?,?)");
+        $statement->execute(array($name, null, null, null, null, null, null, null, null, 8));
+
+        $_SESSION['name'] = $name;
+
+        header("Location: frage1.php");
+        exit();
+    } catch (\Throwable $e) {
+        // Fehlerbehandlung
     }
 }
 ?>
