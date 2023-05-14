c3a20d303b4a9225c338de8c014fabbd52761b50
LucasGreissl <130068605+LucasGreissl@users.noreply.github.com>
2023-05-14 13:05:56 +0200

    Update frage1.php
    Auslagerung von PHP

--- frage1.php
+++ frage1.php
@@ -1,79 +1,42 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-	<meta charset ="utf-8">
-	<meta name = "author" content = "Larissa, Nina, Lucas">
-	<title>Augsburg Quiz</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body class="bodyFrage1Frage2">
-	<div class="quiz-container">
-		<h1>Augsburg Quiz - Frage 1</h1>
-		<hr>
-			<form action = "frage1.php" method = "post" id="quiz-form">
-					<div class="question">
-						<p> Wer hat das Rathaus gebaut?</p>
-						<div class="answers">
-							<label>
-								<input type="radio" value="true" name="Architekten">
-								Elias Holl 
-							</label>
-							<label>
-								<input type="radio" value="false" name="Architekten">
-								Burkhart Engelberg
-							</label>
-							<label>
-								<input type="radio" value="false" name="Architekten">
-								Jakob Fugger
-							</label>		
-						</div>
-					</div>
-				<input type="submit" value="weiter" name="weiter" id="submit-btn">
-			</form>
-	</div>
-
-	<?php
-	session_start();
+<?php
+    session_start();
 	$name = $_SESSION['name'];
+    echo $_SESSION['nameInput'];
 	echo $name;
 
-	if (isset($_POST["weiter"])){
-		
-		if (isset ($_POST['Architekten'])){
-			if ($_POST['Architekten']=="true"){
-				
-				try{
-				$dsn = 'mysql:host=localhost;dbname=quiz;charset=utf8mb4';
-				$username = 'root';
-				$password = '';
-				$dbh = new \PDO($dsn, $username, $password);
-				
-				$statement = $dbh->prepare("UPDATE quizdaten SET frage1 = true WHERE benutzername = '$name'");
-				$statement->execute(); 
-				header("Location: frage2.php");
 
-			}catch(\Throwable $e){
-		
-			}
-			}
-			if ($_POST['Architekten']=="false"){
-				try{
-					$dsn = 'mysql:host=localhost;dbname=quiz;charset=utf8mb4';
-					$username = 'root';
-					$password = '';
-					$dbh = new \PDO($dsn, $username, $password);
-					
-					$statement = $dbh->prepare("UPDATE quizdaten SET frage1 = false WHERE benutzername = '$name'");
-					$statement->execute(); 
-					header("Location: frage2.php");
-	
-				}catch(\Throwable $e){
-			
-				}
-			}
-		}
-	}
-	?>
-	
-</body>
-</html>
+if (isset($_POST['nameInput'])) {
+    $nameInput = $_POST['nameInput'];
+    
+    if ($nameInput == "true"){
+        try{
+        $dsn = 'mysql:host=localhost;dbname=quiz;charset=utf8mb4';
+        $username = 'root';
+        $password = '';
+        $dbh = new \PDO($dsn, $username, $password);
+        
+        $statement = $dbh->prepare("UPDATE quizdaten SET frage1 = true WHERE benutzername = '$name'");
+        $statement->execute(); 
+        
+
+        }catch(\Throwable $e){
+
+        }
+    }else{
+        try{
+            $dsn = 'mysql:host=localhost;dbname=quiz;charset=utf8mb4';
+            $username = 'root';
+            $password = '';
+            $dbh = new \PDO($dsn, $username, $password);
+            
+            $statement = $dbh->prepare("UPDATE quizdaten SET frage1 = false WHERE benutzername = '$name'");
+            $statement->execute(); 
+            
+    
+            }catch(\Throwable $e){
+    
+            }
+    }
+}
+
+?>
