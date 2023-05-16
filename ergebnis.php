d375951c477f1e10fffea625ab380424d390f56b
spNina <nina@spielhaupter-web.de>
2023-05-16 10:31:55 +0200

    Änderungen an Fragen

--- ergebnis.php
+++ ergebnis.php
@@ -1,12 +1,59 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset ="utf-8">
+	<meta name = "author" content = "Larissa, Nina, Lucas">
+	<title>Augsburg Quiz</title>
 </head>
 <body>
-    Ergebnis!!!
+   <h1> Ergebnis!!!</h1>
+
+    <table>
+        <tr>
+         <th>Frage</th>
+         <th>Ergebnis</th>
+        </tr>
+        <tr>
+            <td>Wer hat das Rathaus gebaut?</td>
+            <td id = 'a1'></td>
+        </tr>
+        <tr>
+            <td>Was hat der "Stoinerne Mo" in der Hand?</td>
+            <td id = 'a2'></td>
+        </tr>
+        <tr>
+            <td>In welchem Jahr wurde die Fuggerei von Jakob Fugger gestiftet?</td>
+            <td id = 'a3'></td>
+        </tr>
+        <tr>
+            <td>Welche Nuss ist in Augsburg besonders berühmt?</td>
+            <td id = 'a4'></td>
+        </tr>
+        <tr>
+            <td>Welche Gestalten sind nicht auf dem romanischen Glasfenstern im Augsburger Dom?</td>
+            <td id = 'a5'></td>
+        </tr>
+        <tr>
+            <td>Was sind alles Figuren der Augsburger Puppenkiste?</td>
+            <td id = 'a6'></td>
+        </tr>
+        <tr>
+            <td>Ordnen Sie die Bilder passend per Drag and Drop ins richtige Feld ein!</td>
+            <td id = 'a7'></td>
+        </tr>
+        <tr>
+            <td>Was ist das UNESCO Weltkulturerbe von Augsburg?</td>
+            <td id = 'a8'></td>
+        </tr>
+    </table>
+
+    <?php
+        session_start();
+        $name = $_SESSION['name'];
+        
+        
+
+
+    ?>
 </body>
 </html>
