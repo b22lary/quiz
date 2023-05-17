7365e7864272806f0bc584a33d47cae4f1309b20
LucasGreissl <lucas.greissl@gmail.com>
2023-05-17 10:38:09 +0200

    modified:   DatenbankStruktur 	new file:   ranking.php

---
+++ ranking.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset ="utf-8">
+	<meta name = "author" content = "Larissa, Nina, Lucas">
+	<title>Augsburg Quiz</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+</body>
+<?php
+
+?>
