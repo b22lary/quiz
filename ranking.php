3b5eb3bf4c9840fec06fee55d969702b090634a7
LucasGreissl <lucas.greissl@gmail.com>
2023-05-17 10:50:09 +0200

    modified:   ranking.php

--- ranking.php
+++ ranking.php
@@ -7,9 +7,37 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<table>
+<?php
+    session_start();
+    $name = $_SESSION['name'];
 
+
+    $dsn = 'mysql:host=localhost;dbname=quiz;charset=utf8mb4';
+    $username = 'root';
+    $password = '';
+    $dbh = new \PDO($dsn, $username, $password);
+
+    $conn = mysqli_connect('localhost',$username,$password,'quiz');
+
+    if(!$conn)
+    {
+        die("Verbindung fehlgeschlagen!");
+    }
+
+    $sql = "SELECT * FROM quizdaten ORDER BY (endergebnis) DESC";
+    $result = mysqli_query($conn, $sql);
+
+
+    foreach($dbh-> query($sql) as $row){
+        echo"
+        <tr>
+            <td>" . $row['benutzername'] ."</td>
+            <td>" . $row['endergebnis'] ."</td>
+        </tr>
+        ";
+    }
+?>
+</table>
 
 </body>
-<?php
-
-?>
